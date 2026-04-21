848ae0db3b833bf2179b3260d4846784e92bf07e
rindraniaina <rindraniainarasolofondrasoa17@gmail.com>
2026-04-21 15:18:48 +0300

    feat: Datasource .getConnection method

---
+++ src/main/java/school/hei/federationagricoleapi/datasource/Datasource.java
@@ -0,0 +1,23 @@
+package school.hei.federationagricoleapi.datasource;
+
+import org.springframework.context.annotation.Configuration;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@Configuration
+public class Datasource {
+    public Connection getConnection() {
+        try {
+            String url = System.getenv("DB_URL");
+            String username = System.getenv("DB_USERNAME");
+            String password = System.getenv("DB_PASSWORD");
+
+            return DriverManager.getConnection(url, username, password);
+        }
+        catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
