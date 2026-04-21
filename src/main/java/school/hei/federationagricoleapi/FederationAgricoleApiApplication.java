c22e39a7db891df8b872bc36435725d74ac52d94
jessyrand <henintsoarandriam@gmail.com>
2026-04-21 13:16:34 +0300

    initial commit

---
+++ src/main/java/school/hei/federationagricoleapi/FederationAgricoleApiApplication.java
@@ -0,0 +1,13 @@
+package school.hei.federationagricoleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FederationAgricoleApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FederationAgricoleApiApplication.class, args);
+    }
+
+}
