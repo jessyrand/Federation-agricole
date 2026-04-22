d6d1d5d0a24bf8e05bad91b0402ac1c651334651
rindraniaina <rindraniainarasolofondrasoa17@gmail.com>
2026-04-22 04:32:01 +0300

    feat: initialize database

---
+++ src/main/resources/data/db.sql
@@ -0,0 +1,16 @@
+
+create database agricultural_federation_db;
+
+create user agricultural_federation_db_manager with password '123456';
+
+grant connect on database agricultural_federation to agricultural_federation_db_manager;
+
+\c agricultural_federation_db
+
+grant usage, create on schema public to agricultural_federation_db_manager;
+
+alter default privileges in schema public
+grant select, insert, update, delete on tables to agricultural_federation_db_manager;
+
+alter default privileges in schema public
+grant usage, update, select on sequences to agricultural_federation_db_manager ;
