41807fbb3e54546355bee1978f521f53a8d95d36
rindraniaina <rindraniainarasolofondrasoa17@gmail.com>
2026-04-22 05:33:53 +0300

    feat(repository): find all collectivities

--- src/main/java/school/hei/federationagricoleapi/repository/CollectivityRepository.java
+++ src/main/java/school/hei/federationagricoleapi/repository/CollectivityRepository.java
@@ -6,12 +6,43 @@ import school.hei.federationagricoleapi.entity.Collectivity;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 @AllArgsConstructor
 public class CollectivityRepository {
     private Connection connection;
+    private MemberRepository memberRepository;
+
+    public List<Collectivity> getAllCollectivities() {
+        String sql = """
+                select id, location, president_id, vice_president_id, treasurer_id, secretary_id
+                from collectivities
+                """;
+        List<Collectivity> collectivities = new ArrayList<>();
+
+        try (PreparedStatement pstm = connection.prepareStatement(sql);
+            ResultSet rs = pstm.executeQuery()) {
+            while (rs.next()) {
+                Collectivity collectivity = new Collectivity();
+                collectivity.setId(rs.getString("id"));
+                collectivity.setLocation(rs.getString("location"));
+
+                collectivity.setPresident(memberRepository.findById(rs.getString("president_id")).orElse(null));
+                collectivity.setVicePresident(memberRepository.findById(rs.getString("vice_president_id")).orElse(null));
+                collectivity.setTreasurer(memberRepository.findById(rs.getString("treasurer_id")).orElse(null));
+                collectivity.setSecretary(memberRepository.findById(rs.getString("secretary_id")).orElse(null));
+                collectivities.add(collectivity);
+            }
+            return collectivities;
+        }
+        catch (SQLException e) {
+           throw  new RuntimeException(e);
+        }
+    }
 
     public void save (Collectivity collectivity) {
         String sql = """
