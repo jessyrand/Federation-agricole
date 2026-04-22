6772f56e57425f3b422c4a813fa4d9ed78d9fff1
rindraniaina <rindraniainarasolofondrasoa17@gmail.com>
2026-04-22 06:24:01 +0300

    chore(CollectivityService): create return the list of created collectivity

--- src/main/java/school/hei/federationagricoleapi/service/CollectivityServices.java
+++ src/main/java/school/hei/federationagricoleapi/service/CollectivityServices.java
@@ -9,15 +9,15 @@ import school.hei.federationagricoleapi.entity.Member;
 import school.hei.federationagricoleapi.repository.CollectivityRepository;
 import school.hei.federationagricoleapi.repository.MemberRepository;
 
+import java.util.List;
+
 @Service
 @AllArgsConstructor
 public class CollectivityServices {
     private final CollectivityRepository collectivityRepository;
     private final MemberRepository memberRepository;
 
-    public Collectivity createColectivity(CreateCollectivityDTO collectivity) {
-        String id = collectivityRepository.save(collectivity);
-
-        return collectivityRepository.getById(id);
+    public List<Collectivity> createColectivity(List<CreateCollectivityDTO> collectivity) {
+        return collectivityRepository.save(collectivity);
     }
 }
