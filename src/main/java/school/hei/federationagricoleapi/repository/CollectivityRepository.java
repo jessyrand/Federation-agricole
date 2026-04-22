bd1e395f92fec159dcf33cca61056aea3c47e99e
rindraniaina <rindraniainarasolofondrasoa17@gmail.com>
2026-04-22 05:51:53 +0300

    chore(CollectivityRepository): make it return the id

--- src/main/java/school/hei/federationagricoleapi/repository/CollectivityRepository.java
+++ src/main/java/school/hei/federationagricoleapi/repository/CollectivityRepository.java
@@ -3,6 +3,7 @@ package school.hei.federationagricoleapi.repository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Repository;
 import school.hei.federationagricoleapi.entity.Collectivity;
+import school.hei.federationagricoleapi.entity.DTO.CreateCollectivityDTO;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -44,19 +45,27 @@ public class CollectivityRepository {
         }
     }
 
-    public void save (Collectivity collectivity) {
+    public String save (CreateCollectivityDTO collectivity) {
         String sql = """
                    insert into collectivities (location, president_id, vice_president_id, treasurer_id, secretary_id)
                    values (?, ?, ?, ?, ?)
+                   returning id
                 """;
+        String id;
 
         try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
             pstmt.setString(1, collectivity.getLocation());
-            pstmt.setString(2, collectivity.getPresident().getId());
-            pstmt.setString(3, collectivity.getVicePresident().getId());
-            pstmt.setString(4, collectivity.getTreasurer().getId());
-            pstmt.setString(5, collectivity.getSecretary().getId());
+            pstmt.setString(2, collectivity.getPresident_id());
+            pstmt.setString(3, collectivity.getVicePresident_id());
+            pstmt.setString(4, collectivity.getTreasurer_id());
+            pstmt.setString(5, collectivity.getSecretary_id());
             pstmt.executeUpdate();
+
+            try (ResultSet rs = pstmt.executeQuery()) {
+                if (rs.next()) {
+                    id = rs.getString("id");
+                }
+            }
         }
         catch (SQLException e) {
             throw new RuntimeException(e);
