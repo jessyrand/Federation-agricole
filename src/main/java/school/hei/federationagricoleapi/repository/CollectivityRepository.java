76122a50a5e57bfde0ab291772d742a472e07e21
rindraniaina <rindraniainarasolofondrasoa17@gmail.com>
2026-04-22 06:20:29 +0300

    fix(CollectivityRepository): return outside the loop in save collectivities

--- src/main/java/school/hei/federationagricoleapi/repository/CollectivityRepository.java
+++ src/main/java/school/hei/federationagricoleapi/repository/CollectivityRepository.java
@@ -82,8 +82,8 @@ public class CollectivityRepository {
                        collectivities.add(coll);
                     }
                 }
-                return collectivities;
             }
+            return collectivities;
         }
         catch (SQLException e) {
             throw new RuntimeException(e);
