b460e4843e02b126745d96ddf0dd3036ba346b01
rindraniaina <rindraniainarasolofondrasoa17@gmail.com>
2026-05-07 02:20:35 +0300

    fix: sql + bonus

--- src/main/java/school/hei/federationagricoleapi/repository/CollectivityStatsRepository.java
+++ src/main/java/school/hei/federationagricoleapi/repository/CollectivityStatsRepository.java
@@ -21,44 +21,57 @@ public class CollectivityStatsRepository {
 
     public List<MemberStats> fetchCollectivityStats(String id, LocalDate from, LocalDate to) {
         String sql = """
-                 select m.id, m.first_name, m.last_name, m.email, m.occupation,
-                        (select coalesce(sum(t.amount), 0)
-                         from transaction t
-                         where t.member_debited_id = m.id
-                           and t.transaction_type = 'IN'
-                           and t.creation_date between ? and ?) as earned_amount,
-                     ((select coalesce(sum(mf.amount), 0)
-                       from membership_fee mf
-                       where mf.collectivity_id = cm.collectivity_id
-                         and mf.status = 'ACTIVE'
-                         and mf.eligible_from <= ?)
-                          -
-                      (select coalesce(sum(t.amount), 0)
-                       from transaction t
-                       where t.member_debited_id = m.id
-                         and t.transaction_type = 'IN')) as unpaid_amount,
-                     (select
-                          case when count(ama.id) = 0 then 0.0
-                              else (count(ama.id) filter (where ama.attendance_status = 'ATTENDED')::float 
-                                        / nullif(count(ama.id) filter (where ama.attendance_status in ('ATTENDED', 'MISSING')), 0)) * 100.0
-                              end
-                      from activity_member_attendance ama
-                          join activity a on a.id = ama.activity_id
-                      where ama.member_id = m.id
-                        and a.executive_date between ? and ?) as assiduity_percentage
-                 from member m
-                     join collectivity_member cm on m.id = cm.member_id
-                 where cm.collectivity_id = ?;
-                """;
+            WITH     params AS (
+            SELECT
+                ?::date AS start_d,
+                ?::date AS end_d,
+                ?::varchar AS coll_id)
+            SELECT m.id, m.first_name, m.last_name, m.email, m.occupation,
+                   (SELECT COALESCE(SUM(t.amount), 0)
+                    FROM transaction t
+                        CROSS JOIN params p
+                    WHERE t.member_debited_id = m.id
+                      AND t.transaction_type = 'IN'
+                      AND t.creation_date BETWEEN p.start_d AND p.end_d) AS earned_amount,
+                ((SELECT COALESCE(SUM(
+                 CASE
+                     WHEN mf.frequency = 'ANNUALLY' THEN mf.amount * (EXTRACT(YEAR FROM p.end_d) - EXTRACT(YEAR FROM p.start_d) + 1)
+                     WHEN mf.frequency = 'MONTHLY' THEN mf.amount * ((EXTRACT(YEAR FROM p.end_d) - EXTRACT(YEAR FROM p.start_d)) * 12 +
+                                                                     (EXTRACT(MONTH FROM p.end_d) - EXTRACT(MONTH FROM p.start_d)) + 1)
+                     WHEN mf.frequency = 'WEEKLY' THEN mf.amount * ((p.end_d - p.start_d) / 7 + 1)
+                     ELSE mf.amount
+                     END ), 0)
+                  FROM membership_fee mf
+                      CROSS JOIN params p
+                  WHERE mf.collectivity_id = p.coll_id
+                    AND mf.status = 'ACTIVE'
+                    AND mf.eligible_from <= p.end_d)
+                     -
+                 (SELECT COALESCE(SUM(t.amount), 0)
+                  FROM transaction t
+                  WHERE t.member_debited_id = m.id
+                    AND t.transaction_type = 'IN')) AS unpaid_amount,
+                (SELECT
+                     CASE
+                         WHEN COUNT(ama.id) = 0 THEN 0.0
+                         ELSE (COUNT(ama.id) FILTER (WHERE ama.attendance_status = 'ATTENDED')::float / NULLIF(COUNT(ama.id) FILTER (WHERE ama.attendance_status IN ('ATTENDED', 'MISSING')), 0)) * 100.0
+                         end
+                 FROM activity_member_attendance ama
+                     JOIN activity a ON a.id = ama.activity_id
+                     CROSS JOIN params p
+                 WHERE ama.member_id = m.id
+                   AND a.executive_date BETWEEN p.start_d AND p.end_d) AS assiduity_percentage
+            FROM member m
+                JOIN collectivity_member cm ON m.id = cm.member_id
+                CROSS JOIN params p
+            WHERE cm.collectivity_id = p.coll_id;
+            """;
 
         List<MemberStats> collectivityStats = new ArrayList<>();
         try (PreparedStatement preparedStatement = connection.prepareStatement(sql)){
             preparedStatement.setObject(1, from);
             preparedStatement.setObject(2, to);
-            preparedStatement.setObject(3, to);
-            preparedStatement.setObject(4, from);
-            preparedStatement.setObject(5, to);
-            preparedStatement.setString(6, id);
+            preparedStatement.setString(3, id);
 
            try (ResultSet resultSet = preparedStatement.executeQuery()){
                while (resultSet.next()) {
